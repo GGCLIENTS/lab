d7dadb8575fae36ccad267525cfbb0a509e3668c
noclu <chungusungas@gmail.com>
2023-09-02 12:14:09 +0700

    tooni mashin

---
+++ app.js
@@ -0,0 +1,58 @@
+
+// number, string , boolean , undefined, null
+// console.log("hi" + name)
+// alert();
+// +prompt(); --> garaas utga awah
+//  +, - , * , / --> operator
+// if , else --> herwee buyu uhtsul shalgah
+// >< , <= , >== , ==;
+// && - and bas, || - or erswl
+// var age = +prompt("nasaa bichne uuu")
+// if(age>18 && age<30){
+//     alert("ider nastai hun")
+// }else if(age<=3 && age>=0){
+//     alert("nialh huuhd")
+// }else if(age<18 && age>10){
+//     alert("zalhuu huued")
+// }
+//  1 garaas too awaad tegsh eswel sondgoig shalgah
+// 2 garaas too awhad eyreg eswel surug shalgah
+// switch - NUHTSUL SHALGAN
+// var today = +prompt("too oruul")
+// switch(today){
+//     case 5:
+//         alert("Friday")
+//         break
+//     case 6:
+//         alert("Saturday")
+//         break
+//     case 7:
+//         alert("Sunday")
+//         break
+//     case 1:
+//         alert("monday")
+//         break
+//     case 2:
+//         alert("tuesday")
+//         break
+//     case 3:
+//         alert("wednesday")
+//         break
+//     case 6:
+//         alert("thursday")
+//         break    
+//     default:
+//         alert("not found")
+// }
+// var lang = +prompt("hel oruulna")
+// switch(lang){
+//     case "english":
+//         alert("hello")
+//         break
+//     case "mongolia":
+//         alert("Сайн уу")
+//         break
+//     case "spanish" :
+//         alert("halo")
+//         break
+// }
